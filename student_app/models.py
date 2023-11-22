a2ae6e599283b7d08206bc595699dacfc515b7f8
Derek Marion <derek.j.marion@gmail.com>
2023-11-22 13:34:05 -0800

    Add remove subject methods to Student model

--- student_app/models.py
+++ student_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core import validators as v
-from .validators import validate_name_format, validate_school_email, validate_combination_format
+from .validators import validate_name_format, validate_school_email, validate_combination_format, validate_subjects, validate_subject_format
 
 
 # Create your models here.
@@ -17,7 +17,7 @@ class Student(models.Model):
     )
     locker_combination = models.CharField(default="12-12-12", unique=False, validators=[validate_combination_format])
     good_student = models.BooleanField(default=True, unique=False)
-    subjects = models.ManyToManyField(unique=False, default=None, related_name=students, validators=[v.MinLengthValidator(0), v.MaxLengthValidator(8)])
+    subjects = models.ManyToManyField(Subject, unique=False, default=None, validators=[validate_subjects], related_name='students')
 
     def __str__(self) -> str:
         return f"{self.name} - {self.student_email} - {self.locker_number}"
@@ -40,5 +40,20 @@ class Student(models.Model):
         self.good_student = new_status
         self.save()
 
-    def add_subject(subject_id):
+    def add_subject(self, subject_id):
+        new_subject = validate_subjects(self.subjects)
+        if new_subject:
+            self.subjects.append(subject_id)
+    
+    def remove_subject(self, subject_id):
+        subject_to_remove = validate_subjects(self.subjects)
+        if subject_to_remove:
+            self.subjects.remove(subject_id)
+
+
+class Subject(models.Model):
+    subject_id = models.IntegerField(primary_key=True)
+    subject_name = models.CharField(unique=True, default=None, null=False, validators=[validate_subject_format])
+    professor = models.CharField(unique=False, null=False, default="Mr. Cahan", validators=[validate_professor_name])
+    students = models.ManyToManyField(Student, unique=False, null=False, default=None, validators=[validate_class_capacity])
 
