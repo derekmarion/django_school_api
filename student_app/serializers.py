7f027714059eff96331a6c1a03fa806b5d5f2e86
Derek Marion <derek.j.marion@gmail.com>
2023-11-27 09:36:38 -0800

    Add subjects attribute to student serializer

--- student_app/serializers.py
+++ student_app/serializers.py
@@ -9,15 +9,4 @@ class StudentSerializer(serializers.ModelSerializer):
 class StudentAllSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ['name', 'student_email', 'personal_email', 'locker_number', 'locker_combination', 'good_student']
-
-
-
-# {
-#   "name": "John W. Watson",
-#   "student_email": "thisIsAnEmail@school.com",
-#   "personal_email": "thisIsAnEmail@gmail.com",
-#   "locker_number": 13,
-#   "locker_combination": "12-33-44",
-#   "good_student": true
-# }
+        fields = ['name', 'student_email', 'personal_email', 'locker_number', 'locker_combination', 'good_student', 'subjects']
