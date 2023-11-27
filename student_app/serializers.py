73b4f34aad28336f2b025c8e0d846371d471a34b
Derek Marion <derek.j.marion@gmail.com>
2023-11-27 15:18:17 -0800

    Use SubjectSerializer in StudentAllSerializer to format subject

--- student_app/serializers.py
+++ student_app/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Student
+from subject_app.serializers import SubjectSerializer
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,6 +8,8 @@ class StudentSerializer(serializers.ModelSerializer):
         fields = ['name', 'student_email', 'locker_number']
 
 class StudentAllSerializer(serializers.ModelSerializer):
+    subjects = SubjectSerializer(many=True)
+    
     class Meta:
         model = Student
         fields = ['name', 'student_email', 'personal_email', 'locker_number', 'locker_combination', 'good_student', 'subjects']
