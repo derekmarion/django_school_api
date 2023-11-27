4efc00132f49976999cd31ad6556a40d864ec895
Derek Marion <derek.j.marion@gmail.com>
2023-11-27 08:39:49 -0800

    Add on_delete param to Grade model

--- grade_app/models.py
+++ grade_app/models.py
@@ -5,6 +5,5 @@ from django.core import validators as v
 # Create your models here.
 class Grade(models.Model):
     grade = models.DecimalField(default=100, blank=False, unique=False, validators=[v.MinValueValidator(0.00), v.MaxValueValidator(100.00)])
-    a_subject = models.ForeignKey('subject_app.Subject')
-    student = models.ForeignKey('student_app.Student')
-    
+    a_subject = models.ForeignKey('subject_app.Subject', on_delete=models.CASCADE)
+    student = models.ForeignKey('student_app.Student', on_delete=models.CASCADE)
