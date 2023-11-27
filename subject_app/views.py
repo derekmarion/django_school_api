6df235404753d4fe1ca7b39ba826045a69ce52d9
Derek Marion <derek.j.marion@gmail.com>
2023-11-27 14:31:04 -0800

    Add API view to subject app

--- subject_app/views.py
+++ subject_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Subject
+from .serializers import SubjectSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class All_students(APIView):
+    def get(self, request):
+        subjects = SubjectSerializer(Subject.objects.order_by("id"), many=True)
+        return Response(subjects.data)
