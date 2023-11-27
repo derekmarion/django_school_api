1f83a1a14e24d4c25effcca6c46732cc50b0d490
Derek Marion <derek.j.marion@gmail.com>
2023-11-27 14:24:05 -0800

    Add api endpoint for subjects

--- school_proj/urls.py
+++ school_proj/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/students/', include("student_app.urls"))
+    path('api/v1/students/', include("student_app.urls")),
+    path('api/v1/subjects/', include("subject_app.urls"))
 ]
