f027c80060e756e152e4e98cd152868a11327f36
Derek Marion <derek.j.marion@gmail.com>
2023-11-27 08:42:51 -0800

    Add decimal_places and max_digits to DecimalField

--- grade_app/models.py
+++ grade_app/models.py
@@ -4,6 +4,13 @@ from django.core import validators as v
 
 # Create your models here.
 class Grade(models.Model):
-    grade = models.DecimalField(default=100, blank=False, unique=False, validators=[v.MinValueValidator(0.00), v.MaxValueValidator(100.00)])
-    a_subject = models.ForeignKey('subject_app.Subject', on_delete=models.CASCADE)
-    student = models.ForeignKey('student_app.Student', on_delete=models.CASCADE)
+    grade = models.DecimalField(
+        default=100,
+        blank=False,
+        unique=False,
+        decimal_places=2,
+        max_digits=5,
+        validators=[v.MinValueValidator(0.00), v.MaxValueValidator(100.00)],
+    )
+    a_subject = models.ForeignKey("subject_app.Subject", on_delete=models.CASCADE)
+    student = models.ForeignKey("student_app.Student", on_delete=models.CASCADE)
