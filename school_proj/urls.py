5d1391e19911bf2e471a54eac812d80ff0f54ff7
Derek Marion <derek.j.marion@gmail.com>
2023-11-27 13:33:51 -0800

    fix typo

--- school_proj/urls.py
+++ school_proj/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/students/', include("students_app.urls"))
+    path('api/v1/students/', include("student_app.urls"))
 ]
