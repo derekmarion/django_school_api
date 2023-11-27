8ce46721bc3c6aebf83ea31d4a788986b7be02e9
Derek Marion <derek.j.marion@gmail.com>
2023-11-27 15:10:10 -0800

    add grade average calculation to subject serializer

--- subject_app/serializers.py
+++ subject_app/serializers.py
@@ -3,6 +3,16 @@ from .models import Subject
 
 
 class SubjectSerializer(serializers.ModelSerializer):
+    students = serializers.SerializerMethodField()
+    grade_average = serializers.SerializerMethodField()
+    
     class Meta:
         model = Subject
         fields = ['subject_name', 'professor', 'students', 'grade_average']
+
+    def get_students(self, obj):
+        return obj.students.count()
+    
+    def get_grade_average(self, obj):
+        grades = obj.gradel.all()
+        return round(sum([x.grade for x in grades]/len(grades), 2))
