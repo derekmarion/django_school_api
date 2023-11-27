b3e2d7e9c2edf8d95d32bbea85d68b85e63dc4b8
Derek Marion <derek.j.marion@gmail.com>
2023-11-27 09:11:51 -0800

    Fix import statements for test file

--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
-from grade_app.models import Grade, Student, Subject
+from grade_app.models import Grade
+from student_app.models import Student
+from subject_app.models import Subject
 from django.core.exceptions import ValidationError
 from django.db import IntegrityError, DataError
 from student_app.serializers import StudentAllSerializer, StudentSerializer
