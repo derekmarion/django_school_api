ae27401257c53a976cc5608e223a9e2d3670a124
Derek Marion <derek.j.marion@gmail.com>
2023-11-28 09:23:06 -0800

    Add provided test

--- tests/test_responses.py
+++ tests/test_responses.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 from django.urls import reverse, resolve
-from tests.answers import all_students, all_subjects
+from tests.answers import all_students, all_subjects, a_student, a_subject
 import json
 
 
@@ -37,3 +37,35 @@ class Test_endpoints(TestCase):
     def test_04_url_pattern_students(self):
         url_route = resolve(reverse("all_students"))
         self.assertEquals(url_route.route, 'api/v1/students/')
+
+    # PART VII
+
+    def test_05_get_a_subject(self):
+        response = self.client.get(reverse('a_subject', args=['python']))
+        with self.subTest():
+            self.assert_(response.status_code == 200)
+        content = json.loads(response.content)
+        self.assertEquals(content, a_subject)
+
+    def test_06_get_a_subject_route(self):
+        url_pattern = resolve(reverse('a_subject', args=['python']))
+        self.assertEquals(url_pattern.route, 'api/v1/subjects/<str:subject>/')
+
+    def test_07_get_subject_dne(self):
+        response = self.client.get(reverse('a_subject', args=['psychology']))
+        self.assertEquals(response.status_code, 404)
+
+    def test_08_get_a_student(self):
+        response = self.client.get(reverse('a_student', args=[3]))
+        with self.subTest():
+            self.assert_(response.status_code == 200)
+        content = json.loads(response.content)
+        self.assertEquals(content, a_student)
+
+    def test_09_get_a_student_route(self):
+        url_pattern = resolve(reverse('a_student', args=[3]))
+        self.assertEquals(url_pattern.route, 'api/v1/students/<int:id>/')
+
+    def test_10_get_student_dne(self):
+        response = self.client.get(reverse('a_student', args=[375]))
+        self.assertEquals(response.status_code, 404)
