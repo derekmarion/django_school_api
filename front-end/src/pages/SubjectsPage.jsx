ac03f417d241241e0c45f45626d037ba8f747945
Derek Marion <derek.j.marion@gmail.com>
2023-11-28 12:29:44 -0800

    add api call to retrieve all subjects on subjects page

--- front-end/src/pages/SubjectsPage.jsx
+++ front-end/src/pages/SubjectsPage.jsx
@@ -1,9 +1,47 @@
 import Row from "react-bootstrap/esm/Row";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export const Subjects = () => {
+  const [subjects, setSubjects] = useState([]);
+
+  const getAllSubjects = async () => {
+    let response = await axios
+      .get("http://127.0.0.1:8000/api/v1/subjects/")
+      .catch((err) => {
+        console.log(err);
+        alert("something went wrong");
+      });
+    console.log(response);
+    setSubjects(response.data);
+  };
+
+  useEffect(() => {
+    getAllSubjects();
+  }, []);
+
   return (
     <Row style={{ textAlign: "center", padding: "0 10vmin" }}>
       <h1>Subjects</h1>
+      <ul>
+        {subjects.map((subject) => (
+          // iterates through the list of pokemon to render a li for each student within our database
+          <li
+            key={subject.id}
+            // use the student's id as the key for each "li" element
+            style={{
+              margin: "3vmin",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            Subject: {subject.subject_name} <br />
+            Professor: {subject.professor} <br />
+            Students: {subject.students} <br />
+            Grade Average: {subject.grade_average} <br />
+          </li>
+        ))}
+      </ul>
     </Row>
   );
 };
